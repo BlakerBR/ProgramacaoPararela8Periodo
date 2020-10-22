144e5592fd9068dae5d873705ba8c88e09c7ec4f
BlakerBR <cirofreitas1995@gmail.com>
2020-10-22 15:13:05 -0300

    Organizing for taks2

--- src/Task1/Main.java
+++ src/Task1/Main.java
@@ -1,14 +1,15 @@
-package Task1;
+package task1;
 
+//TASL:
 //This is a task which has the objective to develop an algorithm which uses two threads. 
-//One of them to  print "Programação paralela" on the console, and the second one to print
+//One of them to  print "Programação Paralela" on the console, and the second one to print
 //“Instituto Federal Fluminense” on the console.
 
 public class Main {
 
 	public static void main(String[] args) {
 		Thread thread = new Thread (new ChildThread ());
-		thread.start();
+		thread.start(); 	
 		try {
 			for(int i = 0; i < 10; i++) {
 				System.out.println("Instituto Federal Fluminense");
---
+++ src/task2/Main.java
@@ -0,0 +1,14 @@
+package task2;
+
+// TASL:
+// Consider two numbers a e b (b≠ 0) read by the keyboard and a time ti, 
+// randomly generated from 1 to 20, where ∈{Sum,Sub,Mul,Div}. Develop an 
+// algorithm which creates four threads, where each one will calculate one of 
+// four basic options about a and b: Sum, Subtraction, Multiplication e Division.
+// Besides every operation, each thread should execute other two basic operations,
+// printing the time ti on the screen (in seconds) where it will "sleep"
+// and print it on the screen, after the passing of the ti seconds, which it is then going to end.
+
+public class Main {
+
+}
