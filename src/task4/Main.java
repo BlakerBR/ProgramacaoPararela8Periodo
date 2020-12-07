bedbc00761ba3e7801d54ab49052997a69360781
BlakerBR <cirofreitas1995@gmail.com>
2020-12-07 12:21:02 -0300

    Savepoint

--- src/task4/Main.java
+++ src/task4/Main.java
@@ -1,5 +1,7 @@
 package task4;
 
+import java.lang.Thread.State;
+
 //TASK:
 //Develop an algorithm, using the strategies which were previously implemented (Threads and mechanisms of concurrence
 //control )in order to solve the lower linear triangular system Lx = b where Lx = b is a lower triangular matrix. The 
@@ -9,7 +11,30 @@ package task4;
 public class Main {
 
 	public static void main(String[] args) {
+		int[][] lowerArray = {
+				{2, 0, 0},
+				{1, 4, 0},
+				{1, 1, 1}
+		};
+		int[] vectorB = {2, -3, 1};
+		int[] vectorX = new int[3];
+		int sum;
 		
+		ThreadSum threadSum = new ThreadSum(lowerArray, vectorB);
+		
+		try {
+			threadSum.start();
+		
+			for(int i = 0; i < vectorX.length; i++) {
+				threadSum.notice();
+				while((threadSum.getState() != State.WAITING) && (threadSum.getState() != State.TERMINATED))
+					Thread.sleep(1000);
+				sum = threadSum.getSum();
+				System.out.println(vectorX[i]);
+			}
+		}
+		catch (InterruptedException e) {
+			e.printStackTrace();
+		}
 	}
-	
 }
--- src/task4/ThreadSum.java
+++ src/task4/ThreadSum.java
@@ -2,8 +2,33 @@ package task4;
 
 public class ThreadSum extends Thread {
 	
+	private int[][] lowerArray;
+	private int[] vectorB;
+	private int sum = 0;
+	
+	public ThreadSum(int[][] lowerArray, int[] vectorB) {
+		super();
+		this.lowerArray = lowerArray;
+		this.vectorB = vectorB;
+	}
+
 	public void run() {
-		
+		try {
+			for(int i = 0; i < vectorB.length; i++) {
+				if(i > 0)
+					/*for(int j = 0; j < vectorB.length; j++)
+						sum = sum + (lowerArray[i][j]*);*/
+				
+				startWait();
+			}
+		}
+		catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public synchronized int getSum() {
+		return sum;
 	}
 	
 	private synchronized void startWait() throws InterruptedException {
