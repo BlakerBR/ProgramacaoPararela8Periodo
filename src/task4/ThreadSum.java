5259eddc711ad1efb508b2a27cc6f8ea0628957b
BlakerBR <cirofreitas1995@gmail.com>
2020-12-08 10:14:06 -0300

    Eliminated VectorB from ThreadSum as it was an uncessary attribute

--- src/task4/ThreadSum.java
+++ src/task4/ThreadSum.java
@@ -3,23 +3,21 @@ package task4;
 public class ThreadSum extends Thread {
 	
 	private int[][] lowerArray;
-	private int[] vectorB;
 	private int[] vectorX = new int[3];
 	private int sum;
 	
-	public ThreadSum(int[][] lowerArray, int[] vectorB) {
+	public ThreadSum(int[][] lowerArray) {
 		super();
 		this.lowerArray = lowerArray;
-		this.vectorB = vectorB;
 	}
 
 	public void run() {
 		try {
-			for(int i = 0; i < vectorB.length; i++) {
+			for(int i = 0; i < vectorX.length; i++) {
 				startWait();
 				sum = 0;
 				if(i > 0)
-					for(int j = 0; j < vectorB.length; j++)
+					for(int j = 0; j < vectorX.length; j++)
 						sum = sum + (lowerArray[i][j]*vectorX[j]);
 			}
 		}
